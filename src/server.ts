8ad95b560e01e3ddbba5f71b25d303b6ade0d306
zinoLath <zinolath@gmail.com>
2025-08-20 16:56:29 -0300

    Implement CORS

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'; // Importing the express module
+import cors from 'cors';
 import dotenv from 'dotenv';
 import investimentoController from './controllers/investimento.controller.js';
 const app = express(); // Creating an express app
@@ -6,13 +7,15 @@ const app = express(); // Creating an express app
 dotenv.config();
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
-app.use(express.json());
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors());
 
 app.post('/investments', investimentoController.criar);
 app.get('/investments', investimentoController.listar);
 app.put('/investments/:id', investimentoController.atualizar);
 app.delete('/investments/:id', investimentoController.excluir);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
