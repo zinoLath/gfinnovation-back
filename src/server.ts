382e049843dc2e1a11b9b3e7adc6582be2db7169
zinoLath <zinolath@gmail.com>
2025-08-20 16:33:15 -0300

    Implement routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'; // Importing the express module
 import dotenv from 'dotenv';
+import investimentoController from './controllers/investimento.controller.js';
 const app = express(); // Creating an express app
 
 dotenv.config();
@@ -8,3 +9,10 @@ const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.use(express.json());
+
+app.post('/investments', investimentoController.criar);
+app.get('/investments', investimentoController.listar);
+app.put('/investments/:id', investimentoController.atualizar);
+app.delete('/investments/:id', investimentoController.excluir);
